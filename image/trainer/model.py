373cf7a78df7268f585cff68dfb0d36eaa296c1c
Lukasz_Malucha <lucasmalucha@gmail.com>
2018-08-08 11:47:25 +0000

    z

--- image/trainer/model.py
+++ image/trainer/model.py
@@ -72,6 +72,9 @@ classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = [
 
 from keras.preprocessing.image import ImageDataGenerator 
 
+train_data_dir = 'gs://image-classifier-mlengine/dataset/training_set'
+validation_data_dir = 'gs://image-classifier-mlengine/dataset/test_set'
+
 train_datagen = ImageDataGenerator(rescale=1./255,
                                    shear_range=0.2,
                                    zoom_range=0.2,
@@ -79,12 +82,12 @@ train_datagen = ImageDataGenerator(rescale=1./255,
 
 test_datagen = ImageDataGenerator(rescale=1./255)
 
-training_set = train_datagen.flow_from_directory('gs://image-classifier-mlengine/dataset/training_set',       ## extraction directory
+training_set = train_datagen.flow_from_directory(train_data_dir,       ## extraction directory
                                                  target_size=(96, 96),         ## same dims as in cnn
                                                  batch_size=32,
                                                  class_mode='binary')
 
-test_set = test_datagen.flow_from_directory('gs://image-classifier-mlengine/dataset/test_set',                ## extraction directory
+test_set = test_datagen.flow_from_directory(validation_data_dir,                ## extraction directory
                                             target_size=(96, 96),              ## same dims as in cnn
                                             batch_size=32,
                                             class_mode='binary')
